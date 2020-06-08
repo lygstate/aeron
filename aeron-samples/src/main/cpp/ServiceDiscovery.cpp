3eb289326bab7a1e2cc81a6d18b9ac93abae7a05
Yonggang Luo <luoyonggang@gmail.com>
2020-06-08 11:55:05 +0800

    Add ServiceDiscovery.cpp sample.

---
+++ aeron-samples/src/main/cpp/ServiceDiscovery.cpp
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2014-2020 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cstdio>
+#include <thread>
+
+#define __STDC_FORMAT_MACROS
+
+#include <cinttypes>
+#include <csignal>
+
+#include "util/CommandOptionParser.h"
+#include "concurrent/BusySpinIdleStrategy.h"
+#include "Configuration.h"
+#include "RateReporter.h"
+#include "Aeron.h"
+
+using namespace aeron::util;
+using namespace aeron;
+
+std::atomic<bool> running(true);
+
+void sigIntHandler(int param)
+{
+    running = false;
+}
+
+static const char optHelp     = 'h';
+static const char optPrefix   = 'p';
+static const char optChannel  = 'c';
+static const char optStreamId = 's';
+static const char optMessages = 'm';
+static const char optLength   = 'L';
+
+struct Settings
+{
+    std::string dirPrefix = "";
+    std::string channel = samples::configuration::DEFAULT_CHANNEL;
+    std::int32_t streamId = samples::configuration::DEFAULT_STREAM_ID;
+};
+
+Settings parseCmdLine(CommandOptionParser &cp, int argc, char **argv)
+{
+    cp.parse(argc, argv);
+    if (cp.getOption(optHelp).isPresent())
+    {
+        cp.displayOptionsHelp(std::cout);
+        exit(0);
+    }
+
+    Settings s;
+
+    s.dirPrefix = cp.getOption(optPrefix).getParam(0, s.dirPrefix);
+    s.channel = cp.getOption(optChannel).getParam(0, s.channel);
+    s.streamId = cp.getOption(optStreamId).getParamAsInt(0, 1, INT32_MAX, s.streamId);
+
+    return s;
+}
+
+typedef std::function<int()> on_new_length_t;
+
+static std::random_device randomDevice;
+static std::default_random_engine randomEngine(randomDevice());
+static std::uniform_int_distribution<int> uniformLengthDistribution;
+
+on_new_length_t composeLengthGenerator(bool random, int max)
+{
+    if (random)
+    {
+        std::uniform_int_distribution<int>::param_type param(sizeof(std::int64_t), max);
+        uniformLengthDistribution.param(param);
+
+        return [&]() { return uniformLengthDistribution(randomEngine); };
+    }
+    else
+    {
+        return [max]() { return max; };
+    }
+}
+
+int main(int argc, char **argv)
+{
+    CommandOptionParser cp;
+    cp.addOption(CommandOption(optHelp,     0, 0, "                Displays help information."));
+    cp.addOption(CommandOption(optPrefix,   1, 1, "dir             Prefix directory for aeron driver."));
+    cp.addOption(CommandOption(optChannel,  1, 1, "channel         Channel."));
+    cp.addOption(CommandOption(optStreamId, 1, 1, "streamId        Stream ID."));
+
+    signal(SIGINT, sigIntHandler);
+
+    try
+    {
+        Settings settings = parseCmdLine(cp, argc, argv);
+
+        std::cout << "Discovery at " 
+                  << settings.channel << " on stream ID "
+                  << settings.streamId << std::endl;
+
+        aeron::Context context;
+
+        if (!settings.dirPrefix.empty())
+        {
+            context.aeronDir(settings.dirPrefix);
+        }
+
+        context.newPublicationHandler(
+            [](const std::string &channel, std::int32_t streamId, std::int32_t sessionId, std::int64_t correlationId)
+            {
+                std::cout << "Publication: " << channel << " " << correlationId << ":" << streamId << ":" << sessionId << std::endl;
+            });
+        context.newSubscriptionHandler(
+            [](const std::string &channel, std::int32_t streamId, std::int64_t correlationId)
+            {
+                std::cout << "Subscription: " << channel << " " << correlationId << ":" << streamId << std::endl;
+            });
+
+        context.availableImageHandler(
+            [](Image &image)
+            {
+                std::cout << "Available image correlationId=" << image.correlationId() << " sessionId=" << image.sessionId();
+                std::cout << " at position=" << image.position() << " from " << image.sourceIdentity() << std::endl;
+            });
+
+        context.unavailableImageHandler(
+            [](Image &image)
+            {
+                std::cout << "Unavailable image on correlationId=" << image.correlationId() << " sessionId=" << image.sessionId();
+                std::cout << " at position=" << image.position() << " from " << image.sourceIdentity() << std::endl;
+            });
+        Aeron aeron(context);
+
+        std::int64_t idPublication = aeron.addPublication(settings.channel, settings.streamId);
+
+        std::int64_t idSubscription = aeron.addSubscription(settings.channel, settings.streamId);
+
+        std::shared_ptr<Publication> publication;
+        for(;;)
+        {
+            publication = aeron.findPublication(idPublication);
+            if (publication)
+            {
+                break;;
+            }
+            std::this_thread::yield();
+        }
+
+        std::shared_ptr<Subscription> subscription;
+        for(;;)
+        {
+            subscription = aeron.findSubscription(idSubscription);
+            if (subscription)
+            {
+                break;;
+            }
+            std::this_thread::yield();
+        }
+
+        do
+        {
+            std::this_thread::yield();
+
+        }
+        while (running && continuationBarrier("Execute again?"));
+
+    }
+    catch (const CommandOptionException &e)
+    {
+        std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+        cp.displayOptionsHelp(std::cerr);
+        return -1;
+    }
+    catch (const SourcedException &e)
+    {
+        std::cerr << "FAILED: " << e.what() << " : " << e.where() << std::endl;
+        return -1;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "FAILED: " << e.what() << " : " << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
